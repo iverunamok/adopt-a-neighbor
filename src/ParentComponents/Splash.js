d288a656b341fccd14f47729da42bc18b7a7babb
Shannon Augustine Dooling <shannonadooling@Shannons-MacBook-Pro.local>
2017-07-07 10:16:30 -0600

    Update Helper Chooser and Splash

--- src/ParentComponents/Splash.js
+++ src/ParentComponents/Splash.js
@@ -6,7 +6,8 @@ export default class Splash extends Component {
 	state = {
 		key: 'value'
 	}
-	
+	//this is the front page, if they are a new user, send them to HelperChooser.js, if they are a returning
+  //user, send them to login.js page
 	render(){
 		return( 
   <div>
@@ -20,12 +21,13 @@ export default class Splash extends Component {
           <h3 className="panel-title"></h3>
         </div>
         <div className="panel-body">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          We believe our communities can each help one another to create a united neighborhood that supports and offers hands-on assistance to elders. Joining your community will allow you to send requests for help. You will be linked with a neighbor who is available to help out. Please join the Adopt-A-Neighbor community by clicking on New Neighbor. If you have been here before, click on Returning Neighbor to login.
         </div>
         <div className="panel-footer">
-          <button type="button" className="btn btn-primary">OLD</button>
+            <Link to="/Login"><button type="button" className="btn btn-primary">JOIN NOW</button></Link>
         </div>
-      </div>
+    </div>
+  </div>
       <div className="col-md-6 panel panel-primary panel-default">
       <div className="panel-heading">
         <h3 className="panel-title"></h3>
@@ -37,8 +39,6 @@ export default class Splash extends Component {
         <button type="button" className="btn btn-primary">NEW</button>
       </div>
     </div>
-    </div>
-     
     </div>
 	)}
 }
