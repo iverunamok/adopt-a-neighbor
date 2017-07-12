dbeea641ae361209e2e236f6bc2cb3548f101501
Mike Koosman <mikekoosman@Mikes-MacBook-Pro.local>
2017-07-12 14:52:24 -0600

    MSK - Added Profile Image to SignUpWizard2 Page - 7-12-17

--- src/ParentComponents/SignUpWizard2.js
+++ src/ParentComponents/SignUpWizard2.js
@@ -7,47 +7,32 @@ export default class SignUpWizard2 extends Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			username: "",
+			userName: "",
 			password: "",
 			address: "",
 			profilePic: "",
 			imagePreviewUrl: ""
 		};
 		console.log("Constructor did things")
+		this.userName = this.userName.bind(this)
+		this.passWord = this.passWord.bind(this)
+		this.userAddress = this.userAddress.bind(this)
 	}
 
 	userName(event) {
-		console.log("Set the username to ", this.state.userName)
-		this.setState({username: event.target.value});
+		//console.log("Set the username to ", this.state.userName)
+		this.setState({userName: event.target.value});
 	}
 
 	passWord(event) {
+		//console.log("Password ", this.state.password)
 		this.setState({password: event.target.value});
 	}
 
 	userAddress(event) {
-		this.setState({address: event.target.value});
+		//console.log("address ", this.state.address )
+		this.setState({address: event.target.value})
 	}
-
-// 	_handleImageChange(e) {
-// 	e.preventDefault()
-// 	console.log('yoooooo', e.target.files)
-// 	e.preventDefault();
-//
-// 	let reader = new FileReader();
-// 	let file = e.target.files[0];
-//
-// 	reader.onloadend = () => {
-// 		console.log('setting state')
-// 		this.setState({
-// 			imagePreviewUrl: reader.result
-// 		});
-// 	}
-//
-// 	reader.readAsDataURL(file)
-// }
-
-
 
 	submitLogin(){
 		const fd = new FormData();
@@ -70,16 +55,10 @@ export default class SignUpWizard2 extends Component {
 
 	}
 
-	onPictureChange(e){
-		console.log(e.ref);
-		// this function happens on every hcange opf input
-		// you need to find the ref to the file in the event
-		//  use the reference to the picture file to upload the file to the database
-		//  and set the state of this page, like this -> this.setState({})
-	}
+
+
 	_handleImageChange(e) {
     e.preventDefault()
-    e.preventDefault();
 
     const reader = new FileReader();
     const file = e.target.files[0];
@@ -101,9 +80,9 @@ export default class SignUpWizard2 extends Component {
 
 	render(){
 		const { imagePreviewUrl } = this.state;
-		const $imagePreview = null;
+	  var $imagePreview = null;
 			if (imagePreviewUrl) {
-			$imagePreview = (<img className="previewImage"src={imagePreviewUrl} />);
+			$imagePreview = (<img style={{height: 100, width:100}} className="previewImage"src={imagePreviewUrl} />);
 		} else {
 			$imagePreview = (<div className="previewText"></div>);
 		}
@@ -126,8 +105,8 @@ export default class SignUpWizard2 extends Component {
 								</div>
 
 					<label>Upload a Profile Picture </label>
-					<input onChange={this.onPictureChange.bind(this)} ref="file" type="file"  name="file"/><br/>
-					<input className="file" ref="file" type="file" onChange={(e)=>{console.log('!'); this._handleImageChange(e)}} name="file"/><br/>
+					<input onChange={this._handleImageChange.bind(this)} ref="file" type="file"  name="file"/><br/>
+
 					<div>
 						<button onClick={this.submitLogin.bind(this)}> Submit</button>
 					</div>
@@ -141,3 +120,5 @@ export default class SignUpWizard2 extends Component {
 SignUpWizard2.contextTypes = {
   router: React.PropTypes.object
 }
+
+// goes on line 135: <input className="file" ref="file" type="file" onChange={(e)=>{console.log('!'); this._handleImageChange(e)}} name="file"/><br/>
